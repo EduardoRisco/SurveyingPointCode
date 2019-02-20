2001dc2236790d8463cd1e6e02e919cd6ea89557
EduardoRisco <jrs0069@alu.ubu.es>
2019-02-20 19:23:41 +0100

    Login usuario #12

--- App/src/prototipo_flask/app/route.py
+++ App/src/prototipo_flask/app/route.py
@@ -1,9 +1,14 @@
 from app import app
-from flask import render_template, request, flash, redirect, url_for, send_from_directory
+from flask import render_template, request, flash, redirect, url_for, send_from_directory, send_file
 from werkzeug.utils import secure_filename
 import os
+from app.forms import LoginForm
 
 from app.conversor import genera_dxf
+
+from sqlalchemy import create_engine
+engine = create_engine('postgresql://usr:pass@localhost:5432/sqlalchemy')
+
 
 ALLOWED_EXTENSIONS = set(["txt", "csv"])
 
@@ -12,10 +17,13 @@ def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html', title='Home')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for("upload_file"))
+    return render_template('login.html', title='Sign In', form=form)
 
 
 @app.route("/upload", methods=["GET", "POST"])
@@ -27,15 +35,13 @@ def upload_file():
         if f.filename == "":
             return "No ha seleccionado ningún archivo."
         if f and allowed_file(f.filename):
-            filename = secure_filename(f.filename)
+            filename = secure_filename(f.filename)      
             f.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
             # Se genera y se guarda el archivo dxf
-
             genera_dxf("./tmp/"+filename)
             os.remove("./tmp/"+filename)
             return redirect(url_for("downloads"))
         return "Seleccione un archivo tipo: .txt o .csv."
-
     return render_template('upload.html', title='Carga Archivos')
 
 
