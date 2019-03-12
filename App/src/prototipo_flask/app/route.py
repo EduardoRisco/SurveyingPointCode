2c7b0a10cbdfc8451ce71a8805d671d4ef6385e3
EduardoRisco <jrs0069@alu.ubu.es>
2019-03-12 20:20:35 +0100

    Comienzo con Bootstrap #16

--- App/src/prototipo_flask/app/route.py
+++ App/src/prototipo_flask/app/route.py
@@ -1,318 +1,112 @@
-# -*- coding: utf-8 -*-
-#
-# Prototipo App, conversor a dxf
-#
-# Se requiere PLY (Python Lex-Yacc).
-#
-# J. Eduardo Risco 12-03-2019
-#
 
-import ply.lex as lex
-import ply.yacc as yacc
-
-import ezdxf
-
-# Lexer part
-
-tokens = (
-    "ID",
-    "INT",
-    "FLOAT",
-    "COMA",
-    "COD_GEOM",
-    "COD_ELEM_SING",
-)
-
-reserved = {
-    'I': "COD_GEOM",
-    'IC': "COD_GEOM",
-    'C': "COD_GEOM",
-    'TC': "COD_ELEM_SING",
-    'TR': "COD_ELEM_SING",
-    'TX': "COD_ELEM_SING",
-}
+import os
+from app import app
+from flask import render_template, request, flash, redirect, url_for, send_from_directory, send_file
+from flask_login import LoginManager, logout_user, current_user, login_user, login_required
+from werkzeug.urls import url_parse
+from werkzeug.utils import secure_filename
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from app.forms import RegistrationForm, LoginForm
+from app import db
 
 
-def t_ID(t):
-    r'[a-zA-Z]+'
-    t.type = reserved.get(t.value, 'ID')
-    return t
+from app.conversor import genera_dxf, upload_txt, get_capas, get_errors
+
+ALLOWED_EXTENSIONS = set(["txt", "csv"])
+errores = []
+capas = []
 
 
-def t_FLOAT(t):
-    r'-?([0-9]*\.[0-9]+)'
-    t.value = float(t.value)
-    return t
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-def t_INT(t):
-    r'-?[0-9]+'
-    t.value = int(t.value)
-    return t
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('upload_file'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Error: Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('upload_file')
+            flash('User successfully logged in')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
 
 
-t_COMA = r','
-
-t_ignore = r' '
-
-
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += t.value.count("\n")
-
-
-def t_error(t):
-    raise SyntaxError("syntax error on line %d near '%s'" %
-                      (t.lineno, t.value))
-
-
-lex.lex()
-
-# Parser part
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('upload_file'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 
-def p_linea(p):
-    ''' linea : INT COMA coordenadas COMA codigo '''
-    p[0] = (p[1], p[3]) + p[5]
+@app.route("/upload", methods=["GET", "POST"])
+@login_required
+def upload_file():
+    if request.method == "POST":
+        if not "file" in request.files:
+            flash("Error: No file selected.")
+        f = request.files["file"]
+        if f.filename == "":
+            flash("Error: No file selected.")
+        if f and allowed_file(f.filename):
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            # Se genera y se guarda el archivo dxf
+            upload_txt("./tmp/"+filename)
+            # os.remove("./tmp/"+filename)
+            if get_errors():
+                flash(
+                    'Error: This file has the following errors. Do you want to continue?')
+            return redirect(url_for("convert_file_dxf"))
+        flash("Error: Select a type file: .txt o .csv.")
+    return render_template('upload.html', title='Carga Archivos')
 
 
-def p_coordenadas(p):
-    ''' coordenadas : FLOAT COMA FLOAT COMA FLOAT '''
-    p[0] = p[1], p[3], p[5]
+@app.route("/convert", methods=["GET", "POST"])
+@login_required
+def convert_file_dxf():
+    if request.method == "POST":
+        genera_dxf()
+        return redirect(url_for("downloads"))
+    return render_template('convert.html', title='Conversion DXF', capas=get_capas(), errores=get_errors())
 
 
-def p_codigo(p):
-    ''' codigo : codigo_capa codigo_geometrico
-                   | codigo_elemento_singular codigo_valor_texto
-                   | codigo_capa codigo_no_accesible
-                   | codigo_elemento_singular
-                   | codigo_capa '''
-
-    if len(p) == 3:
-        p[0] = p[1], p[2]
-    else:
-        p[0] = (p[1],)
+@app.route("/download", methods=["GET", "POST"])
+@login_required
+def downloads():
+    if request.method == "POST":
+        return redirect(url_for("download_file", filename='salida.dxf'))
+    return render_template('download.html')
 
 
-def p_codigo_capa(p):
-    ''' codigo_capa : ID '''
-    p[0] = p[1]
-
-
-def p_codigo_geometrico(p):
-    ''' codigo_geometrico : COD_GEOM '''
-    p[0] = p[1]
-
-
-def p_codigo_elemento_singular(p):
-    ''' codigo_elemento_singular :  COD_ELEM_SING '''
-    p[0] = p[1]
-
-
-def p_codigo_valor_texto(p):
-    ''' codigo_valor_texto : FLOAT ID
-                           | INT ID
-                           | FLOAT
-                           | INT
-                           | ID '''
-
-    if len(p) == 3:
-        p[0] = p[1], p[2]
-    else:
-        p[0] = p[1]
-
-
-def p_codigo_no_accesible(p):
-    ''' codigo_no_accesible : FLOAT
-                            | INT 
-                            | codigo_no_accesible FLOAT
-                            | codigo_no_accesible INT  '''
-
-    if len(p) == 3:
-
-        if type(p[1]) == tuple:
-            p[0] = p[1] + (p[2],)
-        else:
-            p[0] = p[1], p[2]
-    else:
-        p[0] = p[1]
-
-
-def p_error(p):
-    if p:
-        return  p.value
-
-
-def upload_txt(entrada):
-
+@app.route('/download_files/<filename>')
+@login_required
+def download_file(filename):
     try:
-        global errores
-        global lineas
-        global curvas
-        global line
-        global err
-        global capas
-
-        parser = yacc.yacc()
-        err = False
-        capas = set()  # Conjunto de capas existentes
-        dicc_capas = {}  # Diccionario que almacena los puntos
-        lineas = []
-        curvas = []
-        puntos = []
-        linea = []
-        curva = []
-        errores=[]
-
-        f = open(entrada)
-        line = f.readline()
-        n_line = 0
-
-        while line != "":
-            n_line += 1
-            # Pasamos el parser
-            punto = parser.parse(line)
-            # Deteccón de archivo de entrada erroneo
-            if not punto:
-                err = True
-                # Captura de errores
-                errores.append(n_line)
-
-            else:
-                codigo_capa = punto[2]
-                # Comprobación que las capas no existan en el diccionario
-                # si no existen se crean
-                # y se añade el primer punto a esa capa
-                if codigo_capa not in capas:
-                    dicc_capas[codigo_capa] = [punto]
-                    capas.add(codigo_capa)
-                else:
-                    # Se añaden los puntos que tengan el mismo codigo
-                    # a su elemento correspondiente en el diccionario
-                    if codigo_capa in dicc_capas:
-                        lista = dicc_capas.get(codigo_capa)
-                        lista.append(punto)
-                        dicc_capas[codigo_capa] = lista
-            line = f.readline()
-
-        # Extracción de lineas y curvas
-
-        for ptos in dicc_capas:
-            linea_iniciada = False
-            curva_iniciada = False
-            for pto in dicc_capas.get(ptos):
-                puntos.append(pto)
-                if pto[2] not in ('TC', 'TR', 'TX'):
-                    if len(pto) > 3:
-                        if pto[3] == 'I':
-                            # Si la linea está iniciada
-                            if linea_iniciada:
-                                # Si encuentro 'I' cierro la linea y empiezo otra
-                                lineas.append(linea)
-                                linea = []
-                                linea.append(pto)
-                                linea_iniciada = True
-                            # Si no existe linea en esa capa, se crea la 1ª linea
-                            else:
-                                linea = []
-                                linea.append(pto)
-                                linea_iniciada = True
-                        elif pto[3] == 'IC':
-                            # Si la curva está iniciada
-                            if curva_iniciada:
-                                # Si encuentro 'IC' cierro la curva y empiezo otra
-                                curvas.append(curva)
-                                curva = []
-                                curva.append(pto)
-                                curva_iniciada = True
-                            # Si no existe curva en esa capa, se crea la 1ª curva
-                            else:
-                                curva = []
-                                curva.append(pto)
-                                curva_iniciada = True
-                        # Se añaden puntos a la curva
-                        elif pto[3] == 'C' and curva_iniciada:
-                            curva.append(pto)
-                    # Se añaden puntos a la linea
-                    elif linea_iniciada:
-                        linea.append(pto)
-            # Si no hay mas elementos en la capa, cerramos lineas y curvas
-            if linea:
-                lineas.append(linea)
-                linea = []
-            if curva:
-                curvas.append(curva)
-                curva = []
-
-        f.close()
-
-        print(get_errors())
-        print(get_capas())
-        
-
-    except (IOError, NameError) as e:
-        print(e)    
+        return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+    except Exception as e:
+        return str(e)
 
 
-def genera_dxf():
-
-    
-
-        # Salida archivo correcto
-        if line == "" and not err:
-            dwg = ezdxf.new('AC1015')
-
-            # Se crea el espacio modelo donde se añaden todos los elementos del dibujo
-            msp = dwg.modelspace()
-
-            # Tratamiento de lineas
-            for ptos in lineas:
-                lin_coord = []
-                for coord_puntos in ptos:
-                    # Se extraen solo las coordenadas del punto (x,y,z)
-                    lin_coord.append(coord_puntos[1])
-                # Funcion para añadir lineas al modelo (polylineas)
-                msp.add_lwpolyline(lin_coord)
-
-            # Tratamiento de curvas
-            for ptos in curvas:
-                lin_coord = []
-                for coord_puntos in ptos:
-                    # Se extraen solo las coordenadas del punto (x,y,z)
-                    lin_coord.append(coord_puntos[1])
-                # Funcion para añadir curvas al modelo
-                msp.add_spline(lin_coord)
-
-            #test    
-
-            l = 0
-            c = 0
-            for el in dwg.entities:
-                if type(el) == ezdxf.modern.lwpolyline.LWPolyline:
-                    l = l+1
-                elif type(el) == ezdxf.modern.spline.Spline:
-                    c = c+1
-            print('Se han añadido', l, 'lineas, ',
-                  c, 'curvas, al archivo dxf creado')
-
-            dwg.saveas("./tmp/salida.dxf")
-
-
-        else:
-            # Salida archivo error
-            print('Archivo erroneo')
-            print(get_errors())
-            print(get_capas())
-          
-
-def get_errors():
-
-    if errores:
-        return errores        
-    return False
-
-def get_capas():
-
-    if capas:
-        return capas        
-    return False  
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
